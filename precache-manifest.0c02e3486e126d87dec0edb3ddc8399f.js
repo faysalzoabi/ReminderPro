7ceca5f9168d718122955b4e735e6a03c58929be
Faysal AlZoubi <faysal.zoabi@gmail.com>
2018-12-27 11:01:37 +0100

    Updates

---
+++ precache-manifest.0c02e3486e126d87dec0edb3ddc8399f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e108cf7927b7d3e9e0b7",
+    "url": "/ReminderPro/static/js/runtime~main.e108cf79.js"
+  },
+  {
+    "revision": "d1c63e8fc70695b832d0",
+    "url": "/ReminderPro/static/js/main.d1c63e8f.chunk.js"
+  },
+  {
+    "revision": "edc2cd974044cce6db25",
+    "url": "/ReminderPro/static/js/1.edc2cd97.chunk.js"
+  },
+  {
+    "revision": "d1c63e8fc70695b832d0",
+    "url": "/ReminderPro/static/css/main.746b6c43.chunk.css"
+  },
+  {
+    "revision": "010c5282e0cd09167fb61096302969b9",
+    "url": "/ReminderPro/index.html"
+  }
+];
